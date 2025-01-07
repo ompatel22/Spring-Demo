5e3fa4acf75687fa32dd768aa5fe94567dd5decc
Om Patel <omupatel22@gmail.com>
2025-01-08 00:00:04 +0530

    updated

--- spring-new-demo/src/main/java/org/example/Bike.java
+++ spring-new-demo/src/main/java/org/example/Bike.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Bike implements Vehicle {
     @Override
     public void drive() {
--- spring-new-demo/src/main/java/org/example/Main.java
+++ spring-new-demo/src/main/java/org/example/Main.java
@@ -6,7 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("config/spring.xml");
-        Vehicle vehicle = (Vehicle) context.getBean("vehicle_id");
+        Vehicle vehicle = (Vehicle) context.getBean("bike");
         vehicle.drive();
+
+        Tyre tyre = (Tyre) context.getBean("tyre_idx");
+        System.out.println(tyre.getBrand());
     }
 }
